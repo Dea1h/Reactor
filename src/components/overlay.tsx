401c97c4b8b7d71d3290bb292e5758eb8bc3018b
Krishna <krishnagupts95@gmail.com>
2025-03-27 10:23:34 +0545

    Minor Changes

---
+++ src/components/overlay.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Overlay() {
+  return(
+    <React.Fragment>
+      <div className="overlay">
+      </div>
+    </React.Fragment>
+  )
+}
+
+export default Overlay
