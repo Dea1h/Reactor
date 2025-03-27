fc544df63d72c38f3fbc149c4eb54948e00a563c
Krishna <krishnagupts95@gmail.com>
2025-03-27 23:19:35 +0545

    Finished Overlay

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -1,26 +1,33 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { NavLink } from 'react-router-dom';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faHouse, faMagnifyingGlass, faCartShopping,faBars } from '@fortawesome/free-solid-svg-icons';
 import '../css/navbar.css';
+import Overlay from './overlay.tsx'
 
 function Navbar() {
+  const [toggle,setToggle] = useState(false);
+  const handleToggle = () => {
+    setToggle(true);
+  };
+
   return (
     <>
       <nav>
         <NavLink to='/' className={({ isActive }) => (isActive ? 'active-link' : 'inactive-link')}>
           <FontAwesomeIcon icon={faHouse} />
         </NavLink>
-        <button>
+        <button onClick={handleToggle}>
         <FontAwesomeIcon icon={faMagnifyingGlass} />
         </button>
-        <NavLink to='/cart' className={({ isActive }) => (isActive ? 'active-link' : 'inactive-link')}>
+        <NavLink to='/' className={({ isActive }) => (isActive ? 'active-link' : 'inactive-link')}>
           <FontAwesomeIcon icon={faCartShopping} />
         </NavLink>
-        <NavLink to='/shop' className={({ isActive }) => (isActive ? 'active-link' : 'inactive-link')}>
+        <NavLink to='/' className={({ isActive }) => (isActive ? 'active-link' : 'inactive-link')}>
           <FontAwesomeIcon icon={faBars} />
         </NavLink>
       </nav>
+      { toggle && <Overlay className="overlay"/>}
     </>
   )
 }
--- src/components/overlay.tsx
+++ src/components/overlay.tsx
@@ -1,9 +1,21 @@
 import React from 'react';
+import '../css/overlay.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faXmark, faMagnifyingGlass } from '@fortawesome/free-solid-svg-icons'
 
 function Overlay() {
   return(
     <React.Fragment>
       <div className="overlay">
+        <div className="overlay data input">
+          <button onClick={1+1} className="Xmark">
+            <FontAwesomeIcon icon={faXmark} />
+          </button>
+          <input placeholder="Search"/>
+          <button className="magnify">
+            <FontAwesomeIcon icon={faMagnifyingGlass} />
+          </button>
+        </div>
       </div>
     </React.Fragment>
   )
